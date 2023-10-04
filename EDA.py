98173890276020c29c0c03fe69fa05ed1bcf66af
KaverH <spongyworm@gmail.com>
2023-10-04 14:46:07 +0100

    Cleaned to fit VS Code. Pending compute steps & hr

--- EDA.py
+++ EDA.py
@@ -5,6 +5,7 @@ This script is used for EDA of the project.
 # Import libraries
 import os
 import re
+import polars as pl
 import pandas as pd
 import numpy as np
 import dtale
@@ -15,7 +16,7 @@ import matplotlib.pyplot as plt
 
 def ChangeDir():
     '''
-    Change directory at the beginning and return the directory of "My Documents"
+    Change directory to ./RawData at the beginning and return the directory of "My Documents"
     '''
     dir_old = os.getcwd()    
     os.chdir('../../')      # work under "My Documents"
@@ -24,7 +25,6 @@ def ChangeDir():
     os.chdir(os.path.join(dir_MyDoc, target_path))
 
     return dir_MyDoc
-
 
 def DescriptiveStat(x, t):
     '''
@@ -61,126 +61,102 @@ def ReadParquet(x):
     
     return y.to_pandas()
 
+class IdentifyContradictions:
 
-# Main program
-if __name__ == '__main__':
-
-    dir_mydoc = ChangeDir()
+    def __init__(self, x) -> None:
+        '''
+        Handle contradictory data on target variable
+        x : input dataset (raw data)
+        '''
+        self.x = x
+        pass
     
-    '''
-    Overview of train_events.csv
-    '''
-    file = './train_events.csv'
-    df = DescriptiveStat(file, 'csv')
-    print(df.nunique())
-    print('\n---------- Finished reading train_events.csv ----------')
+    # Determining the time interval 1 - preparing the dataset
+    def contradictions(self):
+        '''
+        Identify the accelerometers with suspicious records. Replace corresponding steps and timestamps with np.nan and save a new .csv file. Return the new dataframe "gp"
+        '''
+        # Identify
+        gp = self.x.groupby('series_id')['step'].count() 
+        print(gp[gp % 2 == 1])
 
-    '''
-    Looking into the details of the data
-    '''
-    d = dtale.show(df)
-    print(d._main_url)
+        target_replace = gp[gp % 2 == 1]
+        df_contradict = pd.DataFrame({'series_id':target_replace.index, 
+                                'night':[20, 30, 10, 7, 17], 
+                                'additional event':['onset', 'wakeup', 'wakeup', 'wakeup', 'wakeup']
+                                })
+        print(df_contradict)
 
-    # Load train_series.parquet
-    # file = './train_series.parquet'
-    # ReadParquet(file)
+        # Replace
+        for i in df_contradict.index:
+            sid = df_contradict['series_id'][i]
+            night = df_contradict['night'][i]
+            event = df_contradict['additional event'][i]
 
-    '''
-    Investigation of the contradictory data on target variable
-    '''
-    # Identify the accelerometers with suspicious records
-    gp = df.groupby('series_id')['step'].count() 
-    print(gp[gp % 2 == 1])
+            self.x.loc[(self.x['series_id'] == sid) & (self.x['night'] == night) & (self.x['event'] == event), 'step'] = np.nan
+            self.x.loc[(self.x['series_id'] == sid) & (self.x['night'] == night) & (self.x['event'] == event), 'timestamp'] = np.nan
+        
+        # Save the updated dataset into .csv
+        self.x.to_csv('./train_events_replacement.csv')
 
-    target_replace = gp[gp % 2 == 1]
-    df_contradict = pd.DataFrame({'series_id':target_replace.index, 
-                              'night':[20, 30, 10, 7, 17], 
-                              'additional event':['onset', 'wakeup', 'wakeup', 'wakeup', 'wakeup']
-                              })
-    df_contradict
+        self.replacedDF = gp
 
-    '''
-    Determining the time interval 1 - preparing the dataset
-    '''
-    # Replace the five timesteps which cause the contradictions
-    for i in df_contradict.index:
-        sid = df_contradict['series_id'][i]
-        night = df_contradict['night'][i]
-        event = df_contradict['additional event'][i]
+    def nightsNoRecord(self):
+        '''
+        Identify nights without records and return the relevant dataframe
+        '''
+        gp = pd.DataFrame({'sid': self.replacedDF['index'], 'step_num': self.replacedDF['values']})
+        gp['empt_night'] = ''
+        print(gp.head())
 
-        df.loc[(df['series_id'] == sid) & (df['night'] == night) & (df['event'] == event), 'step'] = np.nan
-        df.loc[(df['series_id'] == sid) & (df['night'] == night) & (df['event'] == event), 'timestamp'] = np.nan
+        for sid in gp.sid:
+            df_temp = self.x[(self.x.series_id == sid)]
+            idx = gp[gp.sid == sid].index[0]
+            nights = []
 
-    # Delete variables that become unnecessary for the remaining tasks
-    del(target_replace, df_contradict, sid, night, event, i)
-    
-    # Save the updated dataset into .csv
-    df.to_csv('./train_events_replacement.csv')
+            # Check if each night has a pair of steps
+            empty_night = df_temp[df_temp['step'].isna()]['night']
+            empty_night = empty_night.unique()
+            gp.at[idx, 'empt_night'] = empty_night.tolist()
 
-    # Identify nights without records
-    gp = df.groupby('series_id')['step'].count()
-    gp = pd.DataFrame({'sid': gp.index, 'step_num': gp.values})
-    gp['empt_night'] = ''
+            # Count the number of consecutive days that an accelerometer collected records
+            max_night = df_temp.groupby('series_id')['night'].max()[0]
+            gp.at[idx, 'max_night'] = max_night
+            mt_night = gp[gp.sid == sid]['empt_night'].values[0]
 
-
-    for sid in gp.sid:
-        df_temp = df[(df.series_id == sid)]
-        idx = gp[gp.sid == sid].index[0]
-        nights = []
-
-        # Check if each night has a pair of steps
-        empty_night = df_temp[df_temp['step'].isna()]['night']
-        empty_night = empty_night.unique()
-        gp.at[idx, 'empt_night'] = empty_night.tolist()
-
-        # Coding for the number of consecutive days that an accelerometer collected records
-        max_night = df_temp.groupby('series_id')['night'].max()[0]
-        gp.at[idx, 'max_night'] = max_night
-        mt_night = gp[gp.sid == sid]['empt_night'].values[0]
-
-        if bool(mt_night) == True:
-            for i in range(len(mt_night)):
-                if mt_night[i] != 1:
-                    if i == 0:
-                        con_night = mt_night[i] - 1
-                    elif i+1 == len(mt_night) and mt_night[i] < max_night:
+            if bool(mt_night) == True:
+                for i in range(len(mt_night)):
+                    if mt_night[i] != 1:
+                        if i == 0:
+                            con_night = mt_night[i] - 1
+                        elif i+1 == len(mt_night) and mt_night[i] < max_night:
+                            con_night = max_night - mt_night[i]
+                        else:
+                            con_night = mt_night[i] - mt_night[i-1] - 1
+                    elif len(mt_night) == 1:
                         con_night = max_night - mt_night[i]
-                    else:
-                        con_night = mt_night[i] - mt_night[i-1] - 1
-                elif len(mt_night) == 1:
-                    con_night = max_night - mt_night[i]
 
-                nights.append(con_night)
+                    nights.append(con_night)
 
-            gp.at[idx, 'max_cont_night'] = max(nights)
-        else:
-            gp.at[idx, 'max_cont_night'] = max_night
+                gp.at[idx, 'max_cont_night'] = max(nights)
+            else:
+                gp.at[idx, 'max_cont_night'] = max_night
 
-    # Configure the dtype of numeric data
-    gp['step_num'] = gp['step_num'].astype(np.int8)
-    gp['max_night'] = gp['max_night'].astype(np.int8)
-    gp['max_cont_night'] = gp['max_cont_night'].astype(np.int8)
+        # Configure the dtype of numeric data
+        gp['step_num'] = gp['step_num'].astype(np.int8)
+        gp['max_night'] = gp['max_night'].astype(np.int8)
+        gp['max_cont_night'] = gp['max_cont_night'].astype(np.int8)
 
-    gp.to_csv('./trE_cont_nights.csv')
-    del(df_temp, sid, idx, nights, empty_night, max_night, mt_night, i, con_night)
+        gp.to_csv('./trE_cont_nights.csv')
 
-    # Look into the new table "gp"
-    d = dtale.show(gp)
-    print(d._main_url)
+        self.replacedDF = gp
 
+def ExtractDateTime(x):
     '''
-    Determining the time interval 2 - Looking into the dataset
+    Extract the year, month, day, hour and minute
+    x: input dataset
     '''
-    gp['max_cont_night'].describe()
-    percent = pd.DataFrame({'Day': [gp['max_cont_night'].quantile(0.35), gp['max_cont_night'].quantile(0.43)], 'Percentage' : [1-0.35, 1-0.43],
-                        'Number of Samples': [(277*.65), 277*.57]})
-    percent
-
-    '''
-    Exploiting the dataset - creating a new table
-    '''
-    # Extract the year, month, day, hour and minute
-    df_temp = pd.read_csv('./train_events_replacement.csv', index_col=0)
+    df_temp = pd.read_csv(x, index_col=0)
     df_temp = df_temp.dropna()
     df_temp['UTC_timestamp'] = pd.to_datetime(df_temp['timestamp'], utc=True)
 
@@ -191,6 +167,73 @@ if __name__ == '__main__':
     df_temp['minute'] = df_temp['UTC_timestamp'].dt.minute
     df_temp = df_temp.drop('timestamp', axis=1)
     df_temp = df_temp.drop('UTC_timestamp', axis=1)
+
+    return df_temp
+
+def CheckMissingVal(x):
+        '''
+        Check missing entries of each accelerometer. Return the input dataframe, the trE_cont_nights dataframe and the result as a dictionary
+        x : input dataset (.csv)
+        '''
+        df_check = pd.read_csv(x)
+        meter_msval = {}
+        gp = pd.read_csv('./trE_cont_nights.csv', index_col=0)
+        
+        for sid in gp.sid:
+            max_night = gp[gp[gp.sid == sid]].max_night.values[0]
+            all_nights = df_check[df_check(df_check.series_id == sid)]['night'].unique()
+
+            for i in range(1, max_night+1):
+                if i not in all_nights:
+                    print(f'Missing entry identified : \tsid [{sid}] \tnight [{i}]')
+                    if meter_msval.has_key(sid):
+                        meter_msval[sid].append(i)
+                    else:
+                        meter_msval[sid] = [i]
+
+        return df_check, gp, meter_msval
+
+
+# Main program
+if __name__ == '__main__':
+
+    dir_mydoc = ChangeDir()
+    
+    # Overview of train_events.csv
+    file = './train_events.csv'
+    df = DescriptiveStat(file, 'csv')
+    print(df.nunique())
+    print('\n---------- Finished reading train_events.csv ----------')
+
+    # Looking into the details of the data
+    d = dtale.show(df)
+    print(d._main_url)
+
+    # Load train_series.parquet
+    # file = './train_series.parquet'
+    # ReadParquet(file)
+
+    '''
+    Determining the time interval
+    '''
+    # Identify the contradictory steps and timestamps
+    Int = IdentifyContradictions(df)
+    Int.contradictions()
+    Int.nightsNoRecord()
+    dfNoContra = Int.replacedDF
+
+    # Looking into the dataset
+    dfNoContra['max_cont_night'].describe()
+    percent = pd.DataFrame({'Day': [dfNoContra['max_cont_night'].quantile(0.35), dfNoContra['max_cont_night'].quantile(0.43)], 
+                            'Percentage' : [1-0.35, 1-0.43],
+                            'Number of Samples': [(277*.65), 277*.57]
+                            })
+    print(percent)
+
+    '''
+    Exploiting the dataset - creating a new table
+    '''
+    dfUTC = ExtractDateTime('./train_events_replacement.csv')
 
     # Compute the number of steps for each night and store in a new dataframe
     col_sid = []
@@ -214,17 +257,8 @@ if __name__ == '__main__':
                             'step_number': col_diff
                             })
 
-    '''
-    Checking missing values
-    '''
-    df_check = pd.read_csv('./train_events.csv')
-    for sid in gp.sid:
-        max_night = gp[(gp.sid == sid)].max_night.values[0]
-        all_nights = df_check[df_check.series_id == sid]['night'].unique()
+    # Check missing values
+    dfRaw, dfContN, missVal = CheckMissingVal('./train_events.csv')   
 
-        for i in range(1, max_night+1):
-            if i not in all_nights:
-                print(f'Missing entry identified : \tsid [{sid}] \tnight [{i}]')
-        
-
+    
     pass
