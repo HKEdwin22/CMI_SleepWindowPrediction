d900bfb916280b433496b52bce4d9b3376036db6
KaverH <spongyworm@gmail.com>
2023-11-16 04:59:57 +0000

    Ready for submission (use submission2 & model2)

---
+++ submission2.py
@@ -0,0 +1,90 @@
+# Import libraries
+import EDA_sleeplog as es
+import EDA_timeseries as et
+
+import polars as pl
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import Classifier as CLF
+import pickle
+import time as time
+
+# Main program
+if __name__ == '__main__':
+
+    _ = es.ChangeDir()
+
+    startExe = time.time()
+
+    # Load data
+    file = './test_series.parquet'
+    df = pl.scan_parquet('./test_series.parquet').with_columns(
+        (
+            (pl.col('step').cast(pl.UInt32)), 
+            (pl.col('anglez').round(0).cast(pl.Int8))
+        )
+    ).select(['series_id', 'step', 'timestamp', 'anglez']).collect().to_pandas()
+    sid = df.series_id.unique()
+
+    dfAvg = et.RollingAvg(df, sid, 2, 1, 'test')
+    X = dfAvg.iloc[:, 2:]
+
+    scaler = MinMaxScaler()
+    model = scaler.fit(X)
+    X = model.transform(X)
+
+    # Load and deploy the model
+    clf = pickle.load(open('./model2', 'rb'))
+    y_pred = clf.predict(X).tolist()
+
+    # Extract windows that are different from the initial state
+    dfAvg['prediction'] = pd.Series(y_pred)
+    seriesID, step, event = [], [], []
+
+    for s in sid:
+        
+        window = []
+
+        dfRaw = df[df['series_id'] == s].reset_index()
+        tg = dfAvg[dfAvg.sid == s].reset_index()
+        iniS = tg.iloc[0,5] # initial state
+        predS = tg.iloc[:,5]
+
+        for i in range(len(predS)):
+            if predS[i] != iniS:
+                window.append(tg.iloc[i,2])
+                iniS = tg.iloc[i,5]
+
+        # Identify real changes
+        chgRcd = []
+        for i in range(len(window)):
+            if i+1 != len(window):
+                if window[i+1] - window[i] >= 360:
+                    chgRcd.append(window[i])
+            elif tg.iloc[-1,2] - window[i] >=360:
+                chgRcd.append(window[i])
+                                    
+        if chgRcd != []:
+            for i in chgRcd:
+                state0 = tg[tg['window'] == i-1].prediction
+                state1 = tg[tg['window'] == i].prediction
+                if state0 == state1:
+                    print('Error: pending modifications')
+                else:
+                    seriesID.append(s)
+                    step.append((tg[tg['window'] == i]).step)
+                    event.append('wakeup' if state1 == 'awake' else 'onset')
+
+    # Save results into the submission file
+    submission = {'row_id': [i for i in range(len(seriesID))], 
+                  'series_id': seriesID,
+                  'step': step,
+                  'event': event
+                  }
+    submission = pd.DataFrame(submission, index=False)
+    submission.to_csv('./submission.csv')
+
+endExe = time.time()
+print(f'Execution time : {(endExe-startExe):.2f} seconds')
+
+pass
