13c15b1a792b7725b1835695dac3f293adfd7262
KaverH <spongyworm@gmail.com>
2023-11-22 18:13:01 +0000

    Line 96 index=None

--- submission2.py
+++ submission2.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
                   'step': step,
                   'event': event
                   }
-    submission = pd.DataFrame(submission, index=False)
+    submission = pd.DataFrame(submission, index=None)
     submission.to_csv('./submission.csv')
 
 endExe = time.time()
